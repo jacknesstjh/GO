b94d446b25d8424362c7275f15e89a19af8f235a
jacknesstjh <jackness@126.com>
2017-09-19 21:52:08 +0800

    Things change,Roll with it!

--- 02_array/main.go
+++ 02_array/main.go
@@ -8,6 +8,6 @@ func main() {
 	x := [5]int{2, 3, 5, 6, 8}
 	x[0] = 32
 	x[3] = 11
-	fmt.Println("Element is \n", x[0])
-	fmt.Println("Element is \n", x[2])
+	fmt.Printf("Element is %d\n", x[0])
+	fmt.Printf("Element is %d\n", x[2])
 }
